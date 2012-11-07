ef5455b0dc18a17dd43e9351ea9f0373be13ef03
Sergey Doryba <golfersds@gmail.com>
2012-11-07 04:04:56 +0200

    Refactored

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -1,40 +1,34 @@
-class Dictionary  
-
+class Dictionary
   def entries
     @entries ||={}
   end
 
- def add entry
-    case entry
-      when Hash
-        entries.merge!(entry)
-      when String
-        entries[entry] = nil
-    end
- end
-
- def  keywords
-    @entries.keys.sort
- end
-
- def include? word
-    entries.keys.include? word
- end
- 
- def  find partial_word
-  result = {}
-
-  entries.each_pair do |key, value|
-    if key =~ /^#{partial_word}/
-       result[key] = value
-    end 
+  def add entry
+    entry.is_a?(Hash) ? entries.merge!(entry) : entries[entry] = nil 
   end
- result    
- end
 
- def printable
-   entries.map do |key_value|
-    %Q{[#{key_value.first}] "#{key_value.last}"}
-   end.sort.join("\n")
- end
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include? word
+    entries.keys.include? word
+  end
+
+  def find partial_word
+    result = {}
+
+    entries.each_pair do |key, value|
+      if key =~ /^#{partial_word}/
+      result[key] = value
+      end
+    end
+    result
+  end
+
+  def printable
+    entries.map do |key_value|
+      %Q{[#{key_value.first}] "#{key_value.last}"}
+    end.sort.join("\n")
+  end
 end
