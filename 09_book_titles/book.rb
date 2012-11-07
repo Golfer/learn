45c4410db46cc7b9275fa3754fb403d2f7b69e71
Sergey Doryba <golfersds@gmail.com>
2012-11-07 03:45:20 +0200

    Refactored

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -1,18 +1,23 @@
+WORD_SMALL = ['a','an','in','of','the','and']
+class String
+  def capitalize_unless_small
+    WORD_SMALL.include?(self) ? self :self.capitalize    
+  end
+end
+
 class Book
   attr_accessor :title
-
-  def title
-     words = []
-     @title.split(" ").each do |word|
-         words << capitalize_word(word)
-     end
-     words.first.capitalize!
-     words.join(" ")
-  end 
-
-  def capitalize_word(word)
-     val = word
-     val = ["the","a","an","and","in","of"].include?(word) ? word : word.capitalize
-  end 
-   
+  def title=(phraza)
+    @title=capitalize(phraza)
+  end
+  
+  def capitalize(phraza)
+    phraza.split.inject([]) do |zagolovok, word| 
+        if (zagolovok.empty? || word == "i")
+          zagolovok << word.capitalize      
+        else      
+          zagolovok << word.capitalize_unless_small
+        end
+      end.join(" ")
+    end
 end
